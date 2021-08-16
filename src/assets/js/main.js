b23e46e25bb915c7cc324f2422849f1f0afc74b2
tknr <geishagirlskoume@gmail.com>
2021-08-17 08:32:03 +0900

    modified:   public/assets/js/main.min.js 	modified:   src/assets/js/main.js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -53,3 +53,45 @@ if ('addEventListener' in window) {
 	window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-preload\b/, ''); });
 	document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
 }
+// https://qwerty.work/blog/2020/05/adsense-speedup-lazyload.php
+(function(window, document) {
+        function main() {
+                // GoogleAdSense読込み
+                var ad = document.createElement('script');
+                ad.type = 'text/javascript';
+                ad.async = true;
+                // 新コードの場合、サイト運営者IDを書き換えてコメントアウトを外す
+                ad.dataset.adClient = 'ca-pub-812207611214700';
+                ad.src = '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+                var sc = document.getElementsByTagName('script')[0];
+                sc.parentNode.insertBefore(ad, sc);
+        }
+
+        // 遅延読込み
+        var lazyLoad = false;
+        function onLazyLoad() {
+                if (lazyLoad === false) {
+                        // 複数呼び出し回避 + イベント解除
+                        lazyLoad = true;
+                        window.removeEventListener('scroll', onLazyLoad);
+                        window.removeEventListener('mousemove', onLazyLoad);
+                        window.removeEventListener('mousedown', onLazyLoad);
+                        window.removeEventListener('touchstart', onLazyLoad);
+                        window.removeEventListener('keydown', onLazyLoad);
+                        main();
+                }
+        }
+        window.addEventListener('scroll', onLazyLoad);
+        window.addEventListener('mousemove', onLazyLoad);
+        window.addEventListener('mousedown', onLazyLoad);
+        window.addEventListener('touchstart', onLazyLoad);
+        window.addEventListener('keydown', onLazyLoad);
+        window.addEventListener('load', function() {
+                // ドキュメント途中（更新時 or ページ内リンク）
+                if (window.pageYOffset) {
+                        onLazyLoad();
+                }
+                //何もアクションがないときは指定秒数後に読み込み開始（ミリ秒）
+                window.setTimeout(onLazyLoad,3000)
+        });
+})(window, document);
